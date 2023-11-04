bf5867677fed99e6e83653ca347390e8c00128a0
mwormley008 <mwormley008@gmail.com>
2023-11-03 20:34:17 -0500

    first commit

---
+++ breakcamelcase.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/5208f99aee097e6552000148/train/python
+# Break camelCase
+def solution(s):
+    solution_list = []
+    for i in [*s]:
+        if i.isupper() == True:
+            solution_list.append(' '+i)
+        else:
+            solution_list.append(i)
+    return ''.join(solution_list)
